d5e28311398bdfd828368da96592ba91f79cb84d
benruss64@gmail.com <49625187+SurpriseDog@users.noreply.github.com>
2022-07-18 20:34:40 -0500

    redo se.plot

--- se.plot.py
+++ se.plot.py
@@ -4,59 +4,157 @@
 
 import sys
 import json
+import math
 import datetime
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-def get_views(site, question, days=7):
-    "Bin views into weekly increments."
-    dates = []
-    weekly_views = []
-    delta = datetime.timedelta(days=days-0.5)
-    first_record = False
 
-    with open(sys.argv[1], 'r') as f:
+def get_views(filename, site, question):
+    dates = []
+    views = []
+    with open(filename, 'r') as f:
         for line in f.readlines():
             line = json.loads(line)
             date = datetime.datetime.fromtimestamp(line[0])
             if site in line[1] and question in line[1][site]:
-                views = int(line[1][site][question])
+                count = int(line[1][site][question])
+                dates.append(date)
+                views.append(count)
+    return dates, views
 
-                # Print out data for LibreOffice Calc viewing
-                print(date, views)
-                if not first_record:
-                    first_record = True
-                    start_views = views
-                    start_date = date
 
-                if date - start_date >= delta:
-                    dates.append(date)
-                    weekly_views.append(views - start_views)
-                    start_views = views
-                    start_date = date
-    return dates, weekly_views
+def print_views(dates, views, left, right):
+    "Print out data for LibreOffice Calc viewing"
+    print('\n')
+    print(str(left).ljust(16), '\t', right)
+    for date, view in zip(dates, views):
+        print(date, '\t', round(view, 1))
+
+
+def bin_weekly(dates, views_list, days=7):
+    "Bin views into weekly increments."
+    weekly_dates = []
+    weekly_views = []
+    delta = datetime.timedelta(days=days)
+    end_date = dates[-1]
+    end_views = views_list[-1]
+    weekly_dates.append(end_date)
+
+    # Go thru in reverse order so the the last date is the same in both graphs
+    for date, views in zip(reversed(dates), reversed(views_list)):
+        if end_date - date >= delta or (date == weekly_dates[0] and end_date - date >= delta * 0.7):
+            weekly_dates.append(date)
+            increase = end_views - views
+            # Adjust the numbers if more or less than a whole week
+            adj = (end_date - date) / delta
+            increase /= adj
+            weekly_views.append(increase)
+            end_views = views
+            end_date = date
+
+
+    weekly_dates.pop(-1)
+    weekly_dates.reverse()
+    weekly_views.reverse()
+    return weekly_dates, weekly_views
+
+
+
+def format_axis(ax, dates, views):
+    strip = lambda date: datetime.datetime(date.year, date.month, date.day)
+
+    if dates[-1] - dates[0] >= datetime.timedelta(days=7):
+        tics = get_tics(0, (dates[-1] - dates[0]).days)
+        if len(tics) >= 4 and (tics[1] - tics[0]) / (tics[2] - tics[1]) < 0.5:
+            tics = tics[1:]
+        tics = [strip(dates[0]) + datetime.timedelta(days=days) for days in tics]
+    else:
+        tics = [strip(dates[0]), strip(dates[-1])]
+
+    ax.set_xlim([min(tics[0], dates[0]), max(tics[-1], dates[-1])])
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
+    ax.xaxis.set_minor_locator(mdates.DayLocator(interval=1))
+    ax.set_xticks(tics)
+
+
+    # Set the y tics
+    start = int(views[0])
+    end = views[-1]
+    end = int(end + 1) if end % 1 else int(end)
+    scale = math.log(end - start, 10) if end - start > 1 else 1
+    major = int(10 ** int(scale - 1))
+    while True:
+        tics = list(range(start, end + major, major))
+        if len(tics) <= 10:
+            break
+        major *= 2
+    ax.set_yticks(tics)
+
+    # Set the minor tics
+    if len(tics) > 2:
+        minors = tics[1] - tics[0]
+        if minors > 20:
+            minors = 10
+        if minors > 1:
+            ax.yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator(n=minors))
+
+
+def get_tics(start, end):
+    "Try to find well spaced integer tics between start and end"
+    if end - start <= 5:
+        return list(range(start, end + 1))
+    candidates = []
+    for divisions in range(2, 8):
+        interval = (end - start) // divisions
+        tics = list(range(end, start, -interval))
+        gap = (tics[-1] - start) / interval
+        score = gap - (len(tics) / 10)**2
+        tics = [start] + list(reversed(tics))
+        candidates.append([score, tics])
+        # print(round(score,1), interval, tics, '\n')
+    # print('\n')
+    candidates.sort()
+    return candidates[-1][-1]
 
 
 def main():
-    url = sys.argv[2]
+    if len(sys.argv[1:]) >= 2:
+        filename = sys.argv[1]
+        url = sys.argv[2]
+    elif len(sys.argv[1:]) >= 1:
+        filename = 'se.views.out'
+        url = sys.argv[1]
+    else:
+        filename = 'se.views.out'
+        url = input("Input a url to graph: ")
+
     site = url.split('/')[2].split('.')[0]
     question = url.split('/')[4]
 
-    x, y = get_views(site, question)
-    if not x or not y:
-        print("Insufficient data found to make graph.")
+    dates, views = get_views(filename, site, question)
+    if dates:
+        print_views(dates, views, 'Date:', 'Views Total:')
+    else:
+        print("Insufficient data found to make graph")
         return
 
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
-    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=len(x)))
-    plt.locator_params(axis='y', nbins=10)
-    plt.plot(x, y)
-    # plt.scatter(x, y, label= "stars", color= "green", s=30)
-    plt.gcf().autofmt_xdate()
+    weekly_dates, weekly_views = bin_weekly(dates, views)
+    if len(weekly_dates) >= 3:
+        print_views(weekly_dates, weekly_views, 'Date:', "Weekly Views:")
+        fig, (ax1, ax2) = plt.subplots(1, 2)
+        format_axis(ax2, weekly_dates, weekly_views)
+        ax2.set_title('Weekly Views')
+        ax2.plot(weekly_dates, weekly_views)
+    else:
+        fig, ax1 = plt.subplots()
 
-    plt.xlabel('Date')
-    plt.ylabel('Views per week')
-    plt.title(' '.join((site.title(), "Question", question)))
+    format_axis(ax1, dates, views)
+    ax1.set_title('Total Views')
+    ax1.plot(dates, views)
+
+    fig.suptitle('StackExchange Views on ' + ' '.join((site.title(), "Question", question)))
     plt.show()
 
 
